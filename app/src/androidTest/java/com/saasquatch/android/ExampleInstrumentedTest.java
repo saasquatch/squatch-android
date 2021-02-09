37f645dc8507c00ad534466342817beeb40bac5b
slisaasquatch <sli@saasquat.ch>
2021-02-09 14:49:18 -0800

    Code formatting

--- app/src/androidTest/java/com/saasquatch/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/saasquatch/android/ExampleInstrumentedTest.java
@@ -1,13 +1,12 @@
 package com.saasquatch.android;
 
-import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static org.junit.Assert.assertEquals;
 
+import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,10 +15,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-	@Test
-	public void useAppContext() {
-		// Context of the app under test.
-		Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-		assertEquals("com.saasquatch.android", appContext.getPackageName());
-	}
+
+  @Test
+  public void useAppContext() {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    assertEquals("com.saasquatch.android", appContext.getPackageName());
+  }
 }
