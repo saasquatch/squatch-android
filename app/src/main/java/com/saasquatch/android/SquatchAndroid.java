acbd77164f5e825446d58d7198c26d6f29cc3d18
slisaasquatch <sli@saasquat.ch>
2021-02-11 23:27:21 -0800

    Update SquatchAndroid.java

--- app/src/main/java/com/saasquatch/android/SquatchAndroid.java
+++ app/src/main/java/com/saasquatch/android/SquatchAndroid.java
@@ -16,7 +16,6 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import java.io.Closeable;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 /**
