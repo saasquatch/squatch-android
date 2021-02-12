676dd3cbfd20d1ada16c6b3a160fdbb5f89b6cd8
slisaasquatch <sli@saasquat.ch>
2021-02-11 20:14:16 -0800

    Update SquatchAndroid.java

--- app/src/main/java/com/saasquatch/android/SquatchAndroid.java
+++ app/src/main/java/com/saasquatch/android/SquatchAndroid.java
@@ -1,5 +1,7 @@
 package com.saasquatch.android;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import android.annotation.SuppressLint;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,6 +17,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 import java.io.Closeable;
 import java.util.Map;
 import java.util.Objects;
+import org.apache.commons.codec.binary.Base64;
 
 /**
  * Wrapper for {@link SaaSquatchClient} that contains Android specific features.
@@ -57,6 +60,9 @@ public final class SquatchAndroid implements Closeable {
           SquatchJavascriptInterface.applyToWebView(webView);
           final WidgetUpsertResult widgetUpsertResult =
               apiResponse.toModel(WidgetUpsertResult.class);
+          final String template = widgetUpsertResult.getTemplate();
+          final String templateBase64 = Base64.encodeBase64String(template.getBytes(UTF_8));
+          webView.loadData(templateBase64, "text/html", "base64");
         });
   }
 
